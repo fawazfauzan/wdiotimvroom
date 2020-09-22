de23fa80299273b358c4d2ae142e3a67b03bbe02
fawazfauzan <fawaz.f.nahdi@gmail.com>
2020-09-22 16:36:36 +0700

    add config report allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -129,7 +129,10 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: [['allure', {outputDir: 'allure-results'}]],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        useCucumberStepReporter:true
+    }]],
 
 
     //
